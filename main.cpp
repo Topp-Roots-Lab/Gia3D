643f3e4efc872db440b28faa6c72d4872f6cef15
Tim Parker <Tim.ParkerD@gmail.com>
2020-05-02 15:23:38 -0500

    Clean up and convert line endings to Unix

--- main.cpp
+++ main.cpp
@@ -12,16 +12,16 @@ int main(int argc, char ** argv)
         //printf("%d",argc);
 
         // argc includes the name of executable - so, the number of args is "arc-1"
-	if (argc != 4) 
+	if (argc != 4)
 	{
                 //name_of_exe=rsa-skel
-		printf("Usage: rsa-skel [fileinput] [fileoutput] [scale] \nExitting. Try again.\n");
+		printf("Usage: skel [fileinput] [fileoutput] [scale] \nExitting. Try again.\n");
                 // it gives: name_of_exe=/usr/local/bin/skel/skel-2.0
                 //cout << "Usage: " << argv[0] << " [fileinput] [fileoutput] [scale]  \nExitting. Try again.\n" << endl;
 		return 0;
 	}else{
-		cout << "Executing: " << argv[0] << endl;	
-                cout << "fileinput: " << argv[1] << endl;
+		cout << "Executing: " << argv[0] << endl;
+		cout << "fileinput: " << argv[1] << endl;
 		cout << "fileoutput: " << argv[2] << endl;
 		cout << "scale: " << argv[3] << endl;
 		cout << "\nProcessing ... " << endl;
@@ -29,15 +29,15 @@ int main(int argc, char ** argv)
 		string fileinput=argv[1];
 		string fileoutput=argv[2];
 		float scale=(float)atof(argv[3]);
-		skel_and_features_pipeline(fileinput,fileoutput,scale);	
-	}				
-        return 0;	
+		skel_and_features_pipeline(fileinput,fileoutput,scale);
+	}
+        return 0;
 }
 
 
 /*int main(int argc, char ** argv)
 {
-	//if (argc<2) 
+	//if (argc<2)
 	//{
 	//	printf("Inputfile.out and outputfile are needed!.. Exitting.\n");
 	//	return 0;
@@ -45,40 +45,40 @@ int main(int argc, char ** argv)
 	//string filename=argv[1];
 	//string filestorename=argv[2];
 	//// create a root model from the file //rtvox format
-	//MedialCurve skel(filename,1,true);		
-	//writeVisibleIVEff(filestorename,skel);	
+	//MedialCurve skel(filename,1,true);
+	//writeVisibleIVEff(filestorename,skel);
 	//return 1;
 
 
-	
-	if (argc<2) 
+
+	if (argc<2)
 	{
 		printf("This program should be called with RTVOX file as an argument... Exitting.\n");
 		return 0;
 	}
 	string filename=argv[1];
-	
+
 	printf("Chose what you want to do:\n");
-	printf("\t [1] - Read root file, extract the skeleton and compute root features\n");	
-	printf("\t [2] - Read root file, extract and output the skeleton \n");	
-	printf("\t [3] - Compute range of features based on the scaling factor\n");	
+	printf("\t [1] - Read root file, extract the skeleton and compute root features\n");
+	printf("\t [2] - Read root file, extract and output the skeleton \n");
+	printf("\t [3] - Compute range of features based on the scaling factor\n");
 	// read root data, thin and output skeleton as adjacency matrix
-	
+
 	char cmd[10];
 	scanf("%s", cmd);
-	string strcmd=cmd;	
+	string strcmd=cmd;
 	int i;
-	
-	char c;		
+
+	char c;
 
 	for(i=0; i<(int)strcmd.length();i++)
 	{
 		switch(c=strcmd[i])
-		{			
+		{
 			case '1':
-			// read voxelization from file and perform thinning, skeleton prunning, and feature computation 
+			// read voxelization from file and perform thinning, skeleton prunning, and feature computation
 			{
-				skel_and_features(filename);				
+				skel_and_features(filename);
 				break;
 			}
 			case '2':
@@ -86,7 +86,7 @@ int main(int argc, char ** argv)
 			{
 				root_graph_output(filename);
 				break;
-			}	
+			}
 			case '3':
 			// read root data, thin and output skeleton as adjacency matrix
 			{
@@ -96,17 +96,17 @@ int main(int argc, char ** argv)
 			case '4':
 			// read root data, thin and output skeleton as adjacency matrix
 			{
-				printf("You chose to print info about the root:");				
+				printf("You chose to print info about the root:");
 				root_repair(filename);
 				break;
 			}
 			default:
 			{
-				printf("Nothing was chosen... exitting. Ciao!");			
+				printf("Nothing was chosen... exitting. Ciao!");
 			}
 		}
 	}
-    return 0;	
+    return 0;
   }*/
 
 
@@ -118,7 +118,7 @@ int main(int argc, char ** argv)
 
 
 
- 
+
 
 
 
